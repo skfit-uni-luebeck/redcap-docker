56d5e0ce1368ae61856898d17d2682165e5eddbd
Niklas Reimer <niklas.reimer@uksh.de>
2026-02-04 21:07:26 +0100

    update mariadb to 12.1 and php to 8.5

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ readonly SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTPATH
 
-docker pull docker.io/php:8.3-fpm-alpine
+docker pull docker.io/php:8.5-fpm-alpine
 docker compose build php
 docker compose up -d
+docker system prune -f
